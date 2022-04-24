72d8067e4356645d8e33616163f586aa9cc9d867
Daniel <daniel.hirshson@blstsecurity.com>
2022-04-24 11:51:11 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit
 import pandas
 
 df = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.multiselect("Pick some fruits:", list(df.index))
 streamlit.dataframe(df)
 streamlit.title("Diner app title")
 streamlit.header('Breakfast Favorites')
