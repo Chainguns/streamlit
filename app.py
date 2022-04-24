cb364e6fa836814e73c71f76ea621e7a933762ff
Daniel <daniel.hirshson@blstsecurity.com>
2022-04-24 11:48:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 #henlo
 import streamlit
-streamlit.title("Test app title")
+streamlit.title("Diner app title")
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
