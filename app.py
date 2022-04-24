b3bc69cc211d7908e5f5802677960f0071f5da5a
Daniel <daniel.hirshson@blstsecurity.com>
2022-04-24 17:01:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import requests
 import snowflake.connector
 
 
-my_cnx = snowflake.con(**streamlit.secrets["snowflake"])
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
