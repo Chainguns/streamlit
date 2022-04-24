ad05e05a3deecc88eb3b25f938db8b834ac8c958
Daniel <daniel.hirshson@blstsecurity.com>
2022-04-24 11:50:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 #henlo
 import streamlit
+import pandas
+
+df = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(df)
 streamlit.title("Diner app title")
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
