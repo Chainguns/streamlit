01074e7a37588d535d4c05d3069e8f0830aca64c
Daniel <daniel.hirshson@blstsecurity.com>
2022-04-24 15:49:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,16 @@ import streamlit
 import pandas
 import requests
 
-resp = requests.get("https://www.fruityvice.com/api/fruit/watermelon")
+
 streamlit.header('Fruittyvice.com fruit advice!')
-streamlit.text(resp.json())
-df = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-df.set_index('Fruit', inplace=True)
+choice = streamlit.text_input('What fruit would you like advice on?', value='Watermelon')
+streamlit.write('The fruit you chose is: ' + choice)
+resp = requests.get("https://www.fruityvice.com/api/fruit/" + choice)
 normalized = pd.json_normalize(resp.json())
 streamlit.dataframe(normalized)
+
+df = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+df.set_index('Fruit', inplace=True)
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(df.index),['Avocado','Strawberries'])
 fruits_to_show = df.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
